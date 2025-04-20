91307792cea77a86d12ee212dcca0f8ab4c95652
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-04-20 11:41:00 +0700

    push

---
+++ app/src/main/java/com/example/projectbem/DummyData/UserResponse.kt
@@ -0,0 +1,33 @@
+package com.example.projectbem.DummyData
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResponse(
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("user")
+	val user: List<UserItem?>? = null
+)
+
+data class UserItem(
+
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
+
+	@field:SerializedName("nim")
+	val nim: String? = null,
+
+	@field:SerializedName("role")
+	val role: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("divisi")
+	val divisi: Any? = null,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String? = null
+)
