3c64081f0c921c49efe97426b1e83c4ca49780e6
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-04-22 10:04:09 +0700

    Menyelesaikan bagian event

--- app/src/main/java/com/example/projectbem/Adapter/EventAdapter.kt
+++ app/src/main/java/com/example/projectbem/Adapter/EventAdapter.kt
@@ -7,17 +7,28 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.projectbem.Data.EventModel
 import com.example.projectbem.Data.response.event.DataItem
 import com.example.projectbem.R
 
-class EventAdapter(private val list: List<DataItem>) :
+class EventAdapter(private val list: List<DataItem>, private val onItemClick: (DataItem) -> Unit) :
     RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
     inner class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgEvent: ImageView = itemView.findViewById(R.id.imgEvent)
         val txtJudul: TextView = itemView.findViewById(R.id.txtJudulEvent)
         val txtDeskripsi: TextView = itemView.findViewById(R.id.txtDeskripsiEvent)
+        fun bind(event: DataItem) {
+            txtJudul.text = event.judulKegiatan
+            txtDeskripsi.text = event.ringkasan
+
+            Glide.with(itemView.context)
+                .load(event.gambarLogo)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .into(imgEvent)
+
+            itemView.setOnClickListener { onItemClick(event) }
+        }
 
     }
 
@@ -28,15 +39,7 @@ class EventAdapter(private val list: List<DataItem>) :
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
-        val event = list[position]
-        holder.txtJudul.text = event.judulKegiatan
-        holder.txtDeskripsi.text = event.ringkasan
-
-        Glide.with(holder.itemView.context)
-            .load(event.gambarLogo)
-            .placeholder(R.drawable.ic_launcher_background)
-            .error(R.drawable.ic_launcher_background)
-            .into(holder.imgEvent)
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int = list.size
--- app/src/main/java/com/example/projectbem/Home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/projectbem/Home/ui/home/HomeFragment.kt
@@ -1,14 +1,20 @@
 package com.example.projectbem.Home.ui.home
 
+import ApiConfig
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projectbem.Adapter.EventAdapter
-import com.example.projectbem.Data.EventModel
-import com.example.projectbem.R
+import com.example.projectbem.Data.BemRepository
+import com.example.projectbem.Data.UsersViewModel
+import com.example.projectbem.Data.UsersViewModelFactory
+import com.example.projectbem.Data.room.BemDatabase
+import com.example.projectbem.Home.ui.EventsDetailActivity
 import com.example.projectbem.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,6 +24,15 @@ class HomeFragment : Fragment() {
 
     private lateinit var eventAdapter: EventAdapter
 
+    private val viewModel: UsersViewModel by viewModels {
+        UsersViewModelFactory(
+            BemRepository.getInstance(
+                BemDatabase.getInstance(requireContext()).bemDao(),
+                ApiConfig.getApiService(token = null)
+            )
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,8 +40,27 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel.isLoading.observe(viewLifecycleOwner){isLoading ->
+            binding.progressBarrecyclerEvent.visibility = if (isLoading) View.VISIBLE else View.GONE
+        }
+
+        viewModel.events.observe(viewLifecycleOwner){events ->
+            eventAdapter = EventAdapter(events) { event ->
+                val intent = Intent(requireContext(), EventsDetailActivity::class.java)
+                intent.putExtra("EVENT_DATA", event)
+                startActivity(intent)
+            }
+            binding.recyclerEvent.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = eventAdapter
+            }
+        }
+        viewModel.fetchEvents()
     }
 
     override fun onDestroyView() {
