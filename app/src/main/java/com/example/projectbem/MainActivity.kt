564068fa91fd937cef11babb8241e42cf827c3b3
Rifat <ripatfatona@gmail.com>
2025-04-24 09:10:35 +0700

    Gak Tau Add Sama Upload Gak Bisa

---
+++ app/src/main/java/com/example/projectbem/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.projectbem
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectbem.Home.HomeActivity
+import com.example.projectbem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
