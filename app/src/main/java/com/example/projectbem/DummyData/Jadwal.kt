794798c6037cfad5e639a59fb12d59f578fc142c
Rifat <ripatfatona@gmail.com>
2025-04-24 09:35:44 +0700

    Perbaikan Erorr Activity Absensi

---
+++ app/src/main/java/com/example/projectbem/DummyData/Jadwal.kt
@@ -0,0 +1,9 @@
+package com.example.projectbem.DummyData
+
+data class Jadwal(
+    val namaKegiatan: String,
+    val deskripsi: String,
+    val namaDivisi: String,
+    val jamMulai: String,
+    val jamSelesai: String
+)
